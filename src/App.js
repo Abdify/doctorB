baec5e21597b36ea0c8bdc4807074b8c077ccaa3
Abdify <abdullah.fahimm08@gmail.com>
2021-04-12 08:27:48 +0600

    login dashboard allPatients addedd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import { createContext, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AllPatients from './components/AllPatients/AllPatients/AllPatients';
 import Appointment from './components/Appointment/Appointment/Appointment';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
 import Home from './components/Home/Home/Home';
+import Login from './components/Login/Login/Login';
 import NotFound from './components/NotFound/NotFound';
+
+export const UserContext = createContext();
+
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/appointment">
-          <Appointment />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+          <Switch>
+              <Route path="/appointment">
+                  <Appointment />
+              </Route>
+              <Route path="/dashboard/appointment">
+                  <Dashboard></Dashboard>
+              </Route>
+              <Route path="/dashboard/allPatients">
+                  <AllPatients></AllPatients>
+              </Route>
+              <Route path="/login">
+                  <Login></Login>
+              </Route>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+              <Route path="*">
+                  <NotFound />
+              </Route>
+          </Switch>
+      </Router>
+      </UserContext.Provider>
   );
 }
 
